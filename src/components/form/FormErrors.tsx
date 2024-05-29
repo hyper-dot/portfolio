828fedc59d220abdc589201649b1246c0f56e378
Roshan Paudel <rozanpoudel@gmail.com>
2024-05-29 21:19:20 +0545

    👌 IMPROVE: color corrections

--- src/components/form/FormErrors.tsx
+++ src/components/form/FormErrors.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 const FormErrors = ({ children }: { children: ReactNode }) => {
-  return <p className="text-red-500 text-xs">{children}</p>;
+  return <p className="text-destructive text-xs">{children}</p>;
 };
 
 export default FormErrors;
