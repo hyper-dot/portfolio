e7f74b0af02991d52c3c38748885cf9324295b2d
Roshan Paudel <rozanpoudel@gmail.com>
2024-08-03 22:58:55 +0545

    clean

--- src/app/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-
-const layout = ({ children }: { children: ReactNode }) => {
-  return <div className="min-h-[80vh]">{children}</div>;
-};
-
-export default layout;
--- src/components/form/FormErrors.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-
-const FormErrors = ({ children }: { children: ReactNode }) => {
-  return <p className="text-destructive text-xs">{children}</p>;
-};
-
-export default FormErrors;
