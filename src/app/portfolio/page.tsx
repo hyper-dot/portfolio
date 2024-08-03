83ff767472c4c1842e251c799ad52b577297e5db
Roshan Paudel <rozanpoudel@gmail.com>
2024-08-03 23:07:59 +0545

    👌 IMPROVE: stay tuned

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,7 @@
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-1 items-center justify-center text-center text-3xl md:text-5xl">
+      Stay Tuned ...
+    </div>
+  );
 }
