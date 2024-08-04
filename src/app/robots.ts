0c2a2d6934ef28afa40475d2e012a6a33c780f59
Roshan Paudel <rozanpoudel@gmail.com>
2024-08-04 15:34:34 +0545

    👌 IMPROVE: update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,4 +1,5 @@
 import { MetadataRoute } from "next";
+import { BASE_URL } from "@/constants";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -6,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://rosanpaudel.com/sitemap.xml",
+    sitemap: [BASE_URL + "/sitemap.xml", BASE_URL + "/blogs/sitemap.xml"],
   };
 }
