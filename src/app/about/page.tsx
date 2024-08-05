f4041b8aeae643ec05fe4d3f8797cdc2f7a9a7c6
Roshan Paudel <rozanpoudel@gmail.com>
2024-08-05 19:24:13 +0545

    👌 IMPROVE: added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,35 +1,104 @@
 import { FadeIn, FadeInStagger } from "@/components/FadeIn";
+import { data } from "./constants";
+import { cn } from "@/lib/utils";
 
 export default function page() {
-  // return (
-  //   <div className="background-div flex flex-1 items-center justify-center text-center text-3xl md:text-5xl">
-  //     Stay Tuned ...
-  //   </div>
-  // );
-
   return (
     <div className="mt-10">
-      <h2 className="text-center">My Story</h2>
-      <FadeInStagger className="mx-auto mt-10 grid max-w-3xl grid-cols-2">
-        <FadeIn>
-          <div>
-            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          </div>
+      <h1 className="mb-8 text-center text-4xl font-semibold">My Story</h1>
+      <FadeInStagger className="mx-auto mt-10 max-w-3xl space-y-6 px-4">
+        <FadeIn role="contentinfo" className="max-w-lg">
+          I am Roshan Paudel, a passionate Fullstack web developer and Linux
+          enthusiast with expertise in the MERN stack, TypeScript, TypeORM, and
+          PostgreSQL. Currently, I am working as a mid-level frontend developer,
+          excelling in both frontend and backend technologies, including React,
+          Next.js, Node.js, and Express.js.
         </FadeIn>
-        <div />
-        <div />
-        <FadeIn>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-          exercitation amet. Nisi anim cupidatat excepteur officia.
-          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-          consectetur et est culpa et culpa duis.
+        <FadeIn role="contentinfo" className="ml-auto max-w-lg">
+          My experience at <strong>StartupHire</strong> ,{" "}
+          <strong>Metalogic</strong>, and{" "}
+          <strong className="whitespace-nowrap">Sand Console</strong> has
+          equipped me to excel in remote, collaborative environments. I have
+          contributed to numerous high-quality projects, showcasing my ability
+          to create dynamic and responsive web applications. I am committed to
+          continuous learning and thrive on projects that challenge my skills
+          and foster collaboration. I am open to opportunities to apply my
+          expertise, contribute to innovative solutions, and grow
+          professionally.
         </FadeIn>
       </FadeInStagger>
+
+      <div className="mx-auto mb-12 mt-10 flex max-w-3xl flex-col gap-8 px-4">
+        <section className="w-full">
+          <h2 className="mb-6 text-center text-2xl font-semibold">
+            Work Experience
+          </h2>
+
+          <FadeInStagger>
+            {data.workExperience.map((item, index) => (
+              <FadeIn
+                key={index}
+                className="mb-12 flex flex-col items-start gap-y-2 sm:flex-row"
+              >
+                <img
+                  src={item.logo}
+                  alt={item.company}
+                  className={cn(
+                    "mr-6 h-12 w-12 rounded-full",
+                    item.company === "Metalogic Software"
+                      ? "h-[2.6rem] w-[2.6rem]"
+                      : "",
+                  )}
+                />
+                <div className="flex-1">
+                  <h3 className="text-lg font-semibold">{item.company}</h3>
+                  <time className="flex gap-2 text-muted-foreground">
+                    <span>{item.from}</span>
+                    <span className="sm:hidden">-</span>
+                    <svg
+                      viewBox="0 0 341 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="hidden w-[130px] sm:block"
+                    >
+                      <path
+                        d="M340.707 8.70711C341.098 8.31658 341.098 7.68342 340.707 7.29289L334.343 0.928932C333.953 0.538408 333.319 0.538408 332.929 0.928932C332.538 1.31946 332.538 1.95262 332.929 2.34315L338.586 8L332.929 13.6569C332.538 14.0474 332.538 14.6805 332.929 15.0711C333.319 15.4616 333.953 15.4616 334.343 15.0711L340.707 8.70711ZM0 9H340V7H0V9Z"
+                        className="fill-primary/40"
+                      />
+                    </svg>
+                    <span>{item.to}</span>
+                  </time>
+                </div>
+                <div className="text-left">
+                  <h3 className="text-lg font-semibold">{item.title}</h3>
+                  {/*                 <p className="text-gray-600">{item.description}</p> */}
+                </div>
+              </FadeIn>
+            ))}
+          </FadeInStagger>
+        </section>
+
+        <FadeInStagger>
+          <h2 className="mb-6 text-center text-2xl font-semibold">Skills</h2>
+          {Object.entries(data.skills).map(([category, skills], index) => (
+            <FadeIn key={index} className="mb-8">
+              <h3 className="mb-4 text-xl font-medium capitalize">
+                {category}
+              </h3>
+              <ul className="flex flex-wrap gap-4">
+                {skills.map((skill, skillIndex) => (
+                  <li
+                    key={skillIndex}
+                    className="rounded-full bg-secondary px-4 py-2 text-sm"
+                  >
+                    {skill}
+                  </li>
+                ))}
+              </ul>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
+      </div>
     </div>
   );
 }
