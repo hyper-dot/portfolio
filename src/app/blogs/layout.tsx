4cc6151fa56c5dc4ba9529ad2164dc49de4a7b77
Roshan Paudel <rozanpoudel@gmail.com>
2024-09-12 09:43:13 +0545

    👌 IMPROVE: removed scroller button

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,12 +1,5 @@
-import ScrollButton from "@/components/ScrollToTop";
 import { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="background-div flex-1 px-2">
-      {children}
-
-      <ScrollButton />
-    </div>
-  );
+  return <div className="background-div flex-1 px-2">{children}</div>;
 }
