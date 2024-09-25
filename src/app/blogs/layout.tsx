73e83946cfcae167c702e3ed8e68a7f99e817873
Roshan Paudel <rozanpoudel@gmail.com>
2024-09-25 09:31:14 +0545

    👌 IMPROVE: removed bg in blogs pages to remove laggyness

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
-  return <div className="background-div flex-1 px-2">{children}</div>;
+  return <div className="flex-1 px-2">{children}</div>;
 }
