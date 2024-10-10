6734194bc6ca165b89346423cab4b5099cd6e29a
Roshan Paudel <rozanpoudel@gmail.com>
2024-10-10 14:32:36 +0545

    👌 IMPROVE: paragraph color

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,13 +6,14 @@ export default function page() {
   return (
     <div className="mt-10">
       <h1 className="mb-8 text-center text-4xl font-semibold">My Story</h1>
-      <FadeIn className="mx-auto max-w-3xl px-2 text-neutral-700">
+      <FadeIn className="mx-auto max-w-3xl px-2 text-muted-foreground">
         As a developer with full-stack expertise in the <b>MERN </b> stack,
         <b>TypeScript</b> , and <b>PostgreSQL</b> & <b>Mongodb</b> , I&apos;ve
         excelled in remote environments at StartupHire, Metalogic, and Sand
-        Console, creating dynamic web applications. A <b>Linux</b> enthusiast
-        passionate about continuous learning, I seek opportunities to apply my
-        React and Node.js expertise while contributing to innovative solutions.
+        Console, creating dynamic web applications. <br /> A <b>Linux</b>{" "}
+        enthusiast passionate about continuous learning, I seek opportunities to
+        apply my React and Node.js expertise while contributing to innovative
+        solutions.
       </FadeIn>
       <div className="mx-auto mb-12 mt-10 flex max-w-3xl flex-col gap-8 px-4">
         <section className="w-full">
